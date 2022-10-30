d9ca1e262cad1de3113a1413563e566d01ece3cf
akram09 <a.boutouchent@esi-sba.dz>
2022-10-30 19:42:23 +0100

    Initial setup

---
+++ zsh/theme.zsh
@@ -0,0 +1,41 @@
+##
+## Aesthetic Night Colorscheme
+##
+
+# special
+foreground='#313244'
+background='#1e1e2e'
+
+# black
+color0='#181825'
+color8='#1e1e2e'
+
+# red
+color1='#f38ba8'
+color9='#eba0ac'
+
+# green
+color2='#a6e3a1'
+color10='#94e2d5'
+
+# yellow
+color3='#f9e2af'
+color11='#fab387'
+
+# blue
+color4='#89b4fa'
+color12='#74c7ec'
+
+# magenta
+color5='#cba6f7'
+color13='#f5c2e7'
+
+# cyan
+color6='#89dceb'
+color14='#94e2d5'
+
+# white
+color7='#6c7086'
+color15='#7f849c'
+
+# vim:filetype=zsh
